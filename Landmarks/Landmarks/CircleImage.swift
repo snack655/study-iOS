4eebe1bd1c2f059d7dc0b915637df62b7a8be791
최민재 <cmjcmj1230@gmail.com>
2023-10-19 18:04:03 +0900

    study SwiftUI with landmarks tutorial

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 최민재 on 10/19/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 최민재 on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

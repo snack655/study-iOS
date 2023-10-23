e7d49582dd277af92c6b58f864eb19c716f68d70
최민재 <cmjcmj1230@gmail.com>
2023-10-23 15:52:49 +0900

    feat: init weather project

---
+++ MmForecast/MmForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MmForecast
+//
+//  Created by 최민재 on 10/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
